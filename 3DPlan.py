cee411389bf299e7d1c77b272dc6d2d8cebdff72
Thodoris Betsas <betsasth@gmail.com>
2021-04-04 19:54:06 +0300

    Update 3DPlan.py

--- 3DPlan.py
+++ 3DPlan.py
@@ -33,27 +33,32 @@ if __name__ =='__main__':
 
     # --- Define the number of the output images channels ---
     out = ['3D', '4D']
-    out_selection = input_check('3D (0) or 4D (1) output? (Write 0 or 1): ', [0, 1], 'Not a valid answer, please try agiain   ')
+    out_selection = input_check('3D (0) or 4D (1) output? (Write 0 or 1): ', [0, 1], 'Not a valid answer, please try again   ')
     if out_selection == 0:
         message('3D option is selected')
     else:
         message('4D option is selected')
     
     # --- Define the Structure from Motion and Multi View Stereo software --- 
-    SFM_selection = input_check('Agisoft-Metashape (0), Mapillary-OpenSFM (1) or MyTriangulation (2)? (Write 0, 1 or 2): ', [0, 1, 2], 'Not a valid answer, please try agiain')
+    SFM_selection = input_check('Agisoft-Metashape (0), Mapillary-OpenSFM (1) or MyTriangulation (2)? (Write 0, 1 or 2): ', [0, 1, 2], 'Not a valid answer, please try again')
     if SFM_selection == 0:
         message('Agisoft-Metashape option is selected.')
-        agi_selection = input_check('Python Module (0) or GUI (1) output? (Write 0 or 1): ', [0, 1], 'Not a valid answer, please try agiain')
+        agi_selection = input_check('Python Module (0) or GUI (1) output? (Write 0 or 1): ', [0, 1], 'Not a valid answer, please try again')
     elif SFM_selection == 1:
         message('Mapillary-OpenSFM option is selected.')
     else:
         message('MyTriangulation option is selected.')
     
     # --- Define the edge semantic information source ---
-    semantic_selection = input_check('Canny (0) or external semantic information (1)? (Write 0 or 1): ', [0, 1], 'Not a valid answer, please try agiain')
+    semantic_selection = input_check('Canny (0) or external semantic information (1)? (Write 0 or 1): ', [0, 1], 'Not a valid answer, please try again')
     
     imgsuff = input_check('Give the available image format: ', ['.JPG', '.jpg', '.TIFF', '.tiff', '.tif', '.PNG', '.png'], 'The given format is not available or valid.')
     images = find_files(f'{path}/rgb', imgsuff)
+    
+    if len(images) == 0:
+        ans = input_check(f'There are 0 images into rgb directory with {imgsuff} format do you want to continue the process? (y, n) ', ['y', 'Y', 'n', 'N'], 'Not a valid answer, please try again')
+        if ans == 'n' or ans == 'N':
+            error_message('The execution was terminated because the available images are 0', True)
     
     # --- Construct the 4D images ---
     message('Enrich images with semantic information ...')
